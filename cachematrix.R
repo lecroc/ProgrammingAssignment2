82f1600cce095656f59d7b4027b6b5ecac3e175c
LeCrone <charlie.lecrone@compasspoint-mccann.com>
2017-01-26 19:41:50 -0600

    work from 1/26/17

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,52 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makecacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+
+## Test the function
+
+d<-matrix(c(3,0,0,3), 2, 2)  ## Create an invertable matrix
+
+d                            ## Show matrix
+
+solve(d)                     ## use solve() to calculate the inverse of d
+
+s<-makecacheMatrix(d)        ## create cache inverse creating vector
+
+cachesolve(s)                ## calculate initial inverse (no cache)
+
+cachesolve(s)                ## calculate inverse a second time (calls from cache)
+
+d1<-cachesolve(s)            ## store inverted matrix to d1
+
+s1<-makecacheMatrix(d1)      ## create cache inverse creating vector
+
+cachesolve(s1)               ## calculate initial inverse (no cache, should be back to original matrix)
+
+cachesolve(s1)               ## calculate inverse a second time (calls from cache)
