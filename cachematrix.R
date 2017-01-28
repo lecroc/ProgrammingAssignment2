b3c5861a2556425e0e3d01fec4ea14f123548098
LeCrone <charlie.lecrone@compasspoint-mccann.com>
2017-01-28 09:59:13 -0600

    Final Version

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming, Assignment # 2
 
-## Write a short comment describing this function
 
-makecacheMatrix <- function(x = matrix()) {
+## These two functions will take a matrix x as input and through lexical xcoping, allow the inverse of the matrix to be stored in 
+## memory so that it can be called from the cache instead of being recalculated each time it is needed.
+## 
+## The first function, makecacheMatrix reads in the matrix x, creates a NULL value for it's inverse (m), and then creates a list
+## of four functions that can be called from another function.  The values of x and m are retained in the makecacheMatrix environment.
+##
+## The second function, cachesolve, checks to see if the value of m in the makecacheMatrix environment is NULL or not.  If it
+## is, it calculates the inverse value and stores it as m in the makecacheMatrix environment.  If it is not NULL, it just returns
+## the value of m it finds.
+
+
+
+## This function will take a matrix "x" as input and create a list of four new functions that can be
+## called using the "$" operator in another funcion.
+
+makecacheMatrix <- function(x = matrix()) {    # initialize objects x and m
   m <- NULL
-  set <- function(y) {
-    x <<- y
+  set <- function(y) {                         # create set function which assigns values for 
+    x <<- y                                    # x and m (NULL) in the makecacheMatrix environment
     m <<- NULL
   }
-  get <- function() x
-  setinv <- function(solve) m <<- solve
-  getinv <- function() m
-  list(set = set, get = get,
-       setinv = setinv,
+  get <- function() x                          # create get function to get value of x from makecacheMatrix environment
+  setinv <- function(solve) m <<- solve        # create set funcion to set the value of m in makecacheMatix environment
+  getinv <- function() m                       # create get function to get the value of m from makecacheMatrix environment
+  list(set = set, get = get,                   # create a list of the four funcions with names so they can be accessed with 
+       setinv = setinv,                        # $ operator
        getinv = getinv)
 }
 
+## This function requires an argument that has been returned from makecacheMatrix - the list of functions.
+## I checks to see if the value of m is NULL in the makecacheMatrix environment.  If it is not, it returns the value of m.  If it is,
+## it calculates the inverse of the matrix, stores it as m in the makecacheMatrix environment, then returns m.
 
-cachesolve <- function(x, ...) {
-  m <- x$getinv()
-  if(!is.null(m)) {
+cachesolve <- function(x, ...) {              # initialize object that is the list created by makecacheMatrix, along with values
+  m <- x$getinv()                             # for x and m
+  if(!is.null(m)) {                           # if m is not NULL print to console "getting cached data" and return m
     message("getting cached data")
     return(m)
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
-  m
+  data <- x$get()                             # if m is NULL, get matrix x from makecacheMatrix environment
+  m <- solve(data, ...)                       # use solve function for calculate matrix inverse and store if to m
+  x$setinv(m)                                 # use setinv() function from makecacheMatrix to store value of m in makecacheMatrix
+  m                                           # environment and return m
 }
 
 
-## Test the function
+## Test the function using a simple invertable matrix:
 
 d<-matrix(c(3,0,0,3), 2, 2)  ## Create an invertable matrix
 
